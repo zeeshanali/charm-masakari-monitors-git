c2cf1c34fc78f11e9115f3f0ab41ec4d2f7a763f
Zeeshan Ali Shah <javaclinic@gmail.com>
2022-04-04 16:51:53 +0500

    Update test_lib_charm_openstack_masakari_monitors.py

--- unit_tests/test_lib_charm_openstack_masakari_monitors.py
+++ unit_tests/test_lib_charm_openstack_masakari_monitors.py
@@ -54,7 +54,7 @@ class TestMasakariMonitorsCharm(Helper):
 
     def test_config_flags(self):
         charm = self.\
-            _patch_config_and_charm({'config-flags': 'key1=value1, key2=value2'})
+        _patch_config_and_charm({'config-flags':'key1=value1, key2=value2'})
         config = charm.config_flags()
         # Add check here that configuration is as expected.
         self.assertEqual(config.get('key1'), "value1")
